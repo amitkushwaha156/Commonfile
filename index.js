d030c1187095a990f521ffc583e6090500a21b50
amitkushwaha156 <107460209+amitkushwaha156@users.noreply.github.com>
2026-02-23 11:40:33 +0530

    Add console log for button existence check
    Added a console log statement to confirm button existence.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const btn = document.querySelector('.btn');
 
 // Check karein ki button page par exist karta hai ya nahi
 if (btn) {
+    console.log("tested")
     btn.addEventListener('click', function() {
         // Yeh line page ko turant reload kar degi
         window.location.reload(); 
