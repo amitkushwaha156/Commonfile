7cde6d000e8a3fac31c034adde9fe0a7acbc6724
amitkushwaha156 <107460209+amitkushwaha156@users.noreply.github.com>
2026-02-23 11:39:54 +0530

    Implement button click to reload page
    Add button click event to reload the page if button exists.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("test")
+const btn = document.querySelector('.btn');
+
+// Check karein ki button page par exist karta hai ya nahi
+if (btn) {
+    btn.addEventListener('click', function() {
+        // Yeh line page ko turant reload kar degi
+        window.location.reload(); 
+    });
+}
