24f5fbd43912be8a7296d0e3013dba84635e23df
jquerycdnlatest <107460209+jquerycdnlatest@users.noreply.github.com>
2026-02-23 11:52:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const btn = document.querySelector('.btn');
-
-// Check karein ki button page par exist karta hai ya nahi
-if (btn) {
-    console.log("tested")
-    btn.addEventListener('click', function() {
-        // Yeh line page ko turant reload kar degi
-        window.location.reload(); 
-    });
-}
+// 10000 milliseconds = 10 seconds
+setTimeout(function() {
+    window.location.reload();
+}, 10000);
